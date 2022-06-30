7b630179119573538c61db924d672ebe442800d9
thamilarasibca <thamilarasibca@gmail.com>
2022-06-30 13:27:14 +0530

    This is cucumber project

---
+++ features/Login.feature
@@ -0,0 +1,15 @@
+
+@facebook
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @Login
+  Scenario: Login Functionality
+    Given user should be able to open facebook application
+    When user enter username
+    And user enter password
+    And user click on login Button
+    Then user should be able to login successfully
+    
+
+ 
---
+++ src/test/java/com/facebook/test/Loginstepdefinition.java
@@ -0,0 +1,32 @@
+package com.facebook.test;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Loginstepdefinition {
+	@Given("^user should be able to open facebook application$")
+	public void user_should_be_able_to_open_facebook_application() throws Throwable {
+	   
+	}
+
+	@When("^user enter username$")
+	public void user_enter_username() throws Throwable {
+	   
+	}
+
+	@When("^user enter password$")
+	public void user_enter_password() throws Throwable {
+	
+	}
+
+	@When("^user click on login Button$")
+	public void user_click_on_login_Button() throws Throwable {
+	 
+	}
+
+	@Then("^user should be able to login successfully$")
+	public void user_should_be_able_to_login_successfully() throws Throwable {
+	   
+	}
+}
